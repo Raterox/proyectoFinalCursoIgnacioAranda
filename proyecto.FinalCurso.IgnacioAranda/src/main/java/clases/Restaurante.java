cdcb806bafa677f9a71550af90c69fb15af26261
Raterox <92518274+Raterox@users.noreply.github.com>
2022-05-26 18:46:27 +0200

    Clases Terminadas Sin Dao ni Funciones
    Faltaba Restaurante, ya esta completo

--- proyecto.FinalCurso.IgnacioAranda/src/main/java/clases/Restaurante.java
+++ proyecto.FinalCurso.IgnacioAranda/src/main/java/clases/Restaurante.java
@@ -7,4 +7,42 @@ public class Restaurante {
 	private ArrayList<Zona> zonas;
 	private ArrayList<Producto> productoALaVenta;
 	
+	public Restaurante(float ingresosTotales, ArrayList<Zona> zonas, ArrayList<Producto> productoALaVenta) {
+		super();
+		this.ingresosTotales = ingresosTotales;
+		this.zonas = zonas;
+		this.productoALaVenta = productoALaVenta;
+	}
+
+	public float getIngresosTotales() {
+		return ingresosTotales;
+	}
+
+	public void setIngresosTotales(float ingresosTotales) {
+		this.ingresosTotales = ingresosTotales;
+	}
+
+	public ArrayList<Zona> getZonas() {
+		return zonas;
+	}
+
+	public void setZonas(ArrayList<Zona> zonas) {
+		this.zonas = zonas;
+	}
+
+	public ArrayList<Producto> getProductoALaVenta() {
+		return productoALaVenta;
+	}
+
+	public void setProductoALaVenta(ArrayList<Producto> productoALaVenta) {
+		this.productoALaVenta = productoALaVenta;
+	}
+
+	@Override
+	public String toString() {
+		return "Informacion del Restaurante: \n\tIngresos Totales: " + getIngresosTotales() + "\n\tZonas: " + getZonas() + "\n\tProducto A La Venta: "
+				+ getProductoALaVenta();
+	}
+	
+	
 }
