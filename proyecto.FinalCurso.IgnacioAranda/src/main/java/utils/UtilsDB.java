982fbc26c942d0e311cd06bbbbbc42f7be149bc7
Raterox <92518274+Raterox@users.noreply.github.com>
2022-06-04 11:39:35 +0200

    Actualizacion UtilsBD

--- proyecto.FinalCurso.IgnacioAranda/src/main/java/utils/UtilsDB.java
+++ proyecto.FinalCurso.IgnacioAranda/src/main/java/utils/UtilsDB.java
@@ -6,7 +6,7 @@ import java.sql.SQLException;
 import java.sql.Statement;
 
 public abstract class UtilsDB {
-	private static final String cadenaConexion = "jdbc:mysql://localhost/trabajofinalprogramjacion";
+	private static final String cadenaConexion = "jdbc:mysql://localhost/trabajofinalprogramacion";
 	private static final String usuarioBD = "root";
 	private static final String contrasenaBD = "";
 	private static Connection conexion;
