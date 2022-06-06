eb4a8d8a2e3bd805805140796766f567ddd319c7
Raterox <92518274+Raterox@users.noreply.github.com>
2022-06-06 17:41:37 +0200

    Finalizado Sin DAO, nos ponemos con interfaces

--- proyecto.FinalCurso.IgnacioAranda/src/main/java/clases/Producto.java
+++ proyecto.FinalCurso.IgnacioAranda/src/main/java/clases/Producto.java
@@ -2,20 +2,37 @@ package clases;
 
 import java.util.ArrayList;
 
+import enums.Alcoholica;
+import enums.TipoProducto;
 import exception.NombreVacioException;
 import exception.PrecioCeroException;
+import exception.PuestoVacioException;
 
 public class Producto extends EntidadConNombre{
 	private float precio;
 	private ArrayList<Alergeno> alergenos;
+	private TipoProducto tipoProducto;
+	private Alcoholica alcoholica;
 	
-	public Producto(String nombre, float precio, ArrayList<Alergeno> alergenos) throws NombreVacioException, PrecioCeroException {
+	public Producto(String nombre, float precio, ArrayList<Alergeno> alergenos, TipoProducto tipoProducto) throws NombreVacioException, PrecioCeroException, PuestoVacioException {
 		super(nombre);
 		if(precioCero(precio)) {
 			throw new PrecioCeroException("El precio no puede ser cero");
 		}
 		setPrecio(precio);
 		setAlergenos(alergenos);
+		setTipoProducto(tipoProducto);
+	}
+	
+	public Producto(String nombre, float precio, ArrayList<Alergeno> alergenos, TipoProducto tipoProducto, Alcoholica alcoholica) throws NombreVacioException, PrecioCeroException, PuestoVacioException {
+		super(nombre);
+		if(precioCero(precio)) {
+			throw new PrecioCeroException("El precio no puede ser cero");
+		}
+		setPrecio(precio);
+		setAlergenos(alergenos);
+		setTipoProducto(tipoProducto);
+		setAlcoholica(alcoholica);
 	}
 
 	public float getPrecio() {
@@ -34,6 +51,29 @@ public class Producto extends EntidadConNombre{
 		this.alergenos = alergenos;
 	}
 	
+	public TipoProducto getTipoProducto() {
+		return tipoProducto;
+	}
+
+	public void setTipoProducto(TipoProducto tipoProducto) throws PuestoVacioException {
+		if(tipoVacio(tipoProducto)) {
+			throw new PuestoVacioException("Tienes que seleccionar un puesto");
+		}
+		this.tipoProducto = tipoProducto;
+	}
+	
+	public Alcoholica getAlcoholica() {
+		return alcoholica;
+	}
+
+	public void setAlcoholica(Alcoholica alcoholica) {
+		this.alcoholica = alcoholica;
+	}
+
+	private boolean tipoVacio(TipoProducto tipoProducto) {
+		return tipoProducto.toString().isBlank();
+	}
+
 	private boolean precioCero(float precio) {
 		return precio==0;
 	}
--- proyecto.FinalCurso.IgnacioAranda/src/main/java/pantallas/Menu.java
+++ proyecto.FinalCurso.IgnacioAranda/src/main/java/pantallas/Menu.java
@@ -1,5 +1,5 @@
 package pantallas;
-
+//setFont(new Font("Hack Nerd Font", Font.BOLD, 10));
 import javax.swing.JPanel;
 import java.awt.GridBagLayout;
 import javax.swing.JLabel;
@@ -22,58 +22,7 @@ public class Menu extends JPanel{
 	
 	public Menu(Ventana v) {
 		this.ventana=v;
-		GridBagLayout gridBagLayout = new GridBagLayout();
-		gridBagLayout.columnWidths = new int[]{0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
-		gridBagLayout.rowHeights = new int[]{0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
-		gridBagLayout.columnWeights = new double[]{1.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 1.0, Double.MIN_VALUE};
-		gridBagLayout.rowWeights = new double[]{1.0, 0.0, 0.0, 0.0, 1.0, 0.0, 0.0, 0.0, 0.0, 0.0, 1.0, Double.MIN_VALUE};
-		setLayout(gridBagLayout);
 		
-		JLabel lbBienvenido = new JLabel("Bienvenido");
-		lbBienvenido.setFont(new Font("Hack Nerd Font", Font.BOLD, 15));
-		GridBagConstraints gbc_lbBienvenido = new GridBagConstraints();
-		gbc_lbBienvenido.insets = new Insets(0, 0, 5, 5);
-		gbc_lbBienvenido.gridx = 6;
-		gbc_lbBienvenido.gridy = 1;
-		add(lbBienvenido, gbc_lbBienvenido);
-		
-		JLabel lblRestauranteLosCarpinchos = new JLabel("Restaurante Los Carpinchos");
-		lblRestauranteLosCarpinchos.setFont(new Font("Hack Nerd Font", Font.BOLD, 15));
-		GridBagConstraints gbc_lblRestauranteLosCarpinchos = new GridBagConstraints();
-		gbc_lblRestauranteLosCarpinchos.insets = new Insets(0, 0, 5, 5);
-		gbc_lblRestauranteLosCarpinchos.gridx = 6;
-		gbc_lblRestauranteLosCarpinchos.gridy = 2;
-		add(lblRestauranteLosCarpinchos, gbc_lblRestauranteLosCarpinchos);
-		
-		JLabel lbQueQuieresHacer = new JLabel("Que Quieres Hacer?");
-		lbQueQuieresHacer.setFont(new Font("Hack Nerd Font", Font.BOLD, 13));
-		GridBagConstraints gbc_lbQueQuieresHacer = new GridBagConstraints();
-		gbc_lbQueQuieresHacer.insets = new Insets(0, 0, 5, 5);
-		gbc_lbQueQuieresHacer.gridx = 6;
-		gbc_lbQueQuieresHacer.gridy = 7;
-		add(lbQueQuieresHacer, gbc_lbQueQuieresHacer);
-		
-		JButton btnLogin = new JButton("Iniciar Sesion");
-		GridBagConstraints gbc_btnLogin = new GridBagConstraints();
-		gbc_btnLogin.insets = new Insets(0, 0, 5, 5);
-		gbc_btnLogin.gridx = 5;
-		gbc_btnLogin.gridy = 8;
-		add(btnLogin, gbc_btnLogin);
-		btnLogin.addMouseListener(new MouseAdapter() {
-			@Override
-			public void mouseClicked(MouseEvent e) {
-				
-			}
-		});
-		btnLogin.setFont(new Font("Hack Nerd Font", Font.BOLD, 10));
-		
-		JButton btnRegister = new JButton("Registrarse");
-		GridBagConstraints gbc_btnRegister = new GridBagConstraints();
-		gbc_btnRegister.insets = new Insets(0, 0, 5, 5);
-		gbc_btnRegister.gridx = 7;
-		gbc_btnRegister.gridy = 8;
-		add(btnRegister, gbc_btnRegister);
-		btnRegister.setFont(new Font("Hack Nerd Font", Font.BOLD, 10));
 
 	}
 }
