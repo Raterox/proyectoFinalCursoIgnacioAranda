7c5bc645cffc3c72530317cf94e13e661790ab4b
Raterox <92518274+Raterox@users.noreply.github.com>
2022-06-06 15:42:59 +0200

    Limpi

--- proyecto.FinalCurso.IgnacioAranda/src/main/java/clases/Alergeno.java
+++ proyecto.FinalCurso.IgnacioAranda/src/main/java/clases/Alergeno.java
@@ -3,12 +3,13 @@ package clases;
 import java.sql.SQLException;
 
 import exception.NombreVacioException;
+import exception.SimboloVacioException;
 
 //TODO IMPLEMENTAR DAO
 public class Alergeno extends EntidadConNombre{
 	private char simbolo;
 
-	public Alergeno(String nombre, char simbolo) throws SQLException, NombreVacioException {
+	public Alergeno(String nombre, char simbolo) throws SQLException, NombreVacioException, SimboloVacioException {
 		super(nombre);
 		this.simbolo = simbolo;
 	}
