d06b311618334f062aa1d88bcd4b36822a759755
Raterox <92518274+Raterox@users.noreply.github.com>
2022-06-08 23:56:22 +0200

    Update Inicio.java

--- proyecto.FinalCurso.IgnacioAranda/src/main/java/pantallas/Inicio.java
+++ proyecto.FinalCurso.IgnacioAranda/src/main/java/pantallas/Inicio.java
@@ -4,10 +4,35 @@ import java.awt.SystemColor;
 
 import javax.swing.JPanel;
 
+import elementosVisuales.Boton;
+
+import java.awt.FlowLayout;
+import java.awt.CardLayout;
+import javax.swing.JButton;
+import javax.swing.GroupLayout;
+import javax.swing.GroupLayout.Alignment;
+import javax.swing.BoxLayout;
+import java.awt.GridBagLayout;
+import java.awt.GridBagConstraints;
+import java.awt.Insets;
+
 public class Inicio extends JPanel{
 	private Ventana ventana;
 	public Inicio(Ventana v) {
 		this.ventana=v;
 		setBackground(SystemColor.activeCaption);
+		GridBagLayout gridBagLayout = new GridBagLayout();
+		gridBagLayout.columnWidths = new int[]{0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
+		gridBagLayout.rowHeights = new int[]{0, 0, 0, 0};
+		gridBagLayout.columnWeights = new double[]{1.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 1.0, Double.MIN_VALUE};
+		gridBagLayout.rowWeights = new double[]{0.0, 0.0, 0.0, Double.MIN_VALUE};
+		setLayout(gridBagLayout);
+		
+		Boton btnNewButton = new Boton("New button");
+		GridBagConstraints gbc_btnNewButton = new GridBagConstraints();
+		gbc_btnNewButton.insets = new Insets(0, 0, 5, 5);
+		gbc_btnNewButton.gridx = 4;
+		gbc_btnNewButton.gridy = 1;
+		add(btnNewButton, gbc_btnNewButton);
 	}
 }
